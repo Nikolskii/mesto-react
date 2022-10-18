157e55232f8f39ee08161bd5b20054625412f30a
Denis Nikolskiy <den-apple@ya.ru>
2022-10-18 20:39:57 +0200

    chore: useContext currentUser in Main

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import '../index.css';
 
 function App() {
@@ -56,124 +57,126 @@ function App() {
 
   return (
     <div className="page">
-      <Header />
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header />
 
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        onCardClick={handleCardClick}
-      />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+        />
 
-      <Footer />
+        <Footer />
 
-      {/* Попап редактирования профиля */}
-      <PopupWithForm
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-        title="Редактировать профиль"
-        name="edit-profile"
-        buttonText="Сохранить"
-      >
-        <fieldset className="form__fieldset">
-          <input
-            className="form__input form__input_type_name"
-            placeholder="Имя пользователя"
-            type="text"
-            name="form__input_type_name"
-            id="form__input_type_name"
-            minLength="2"
-            maxLength="40"
-            required
-          />
-          <span
-            className="form__input-error"
-            id="form__input_type_name-error"
-          ></span>
-          <input
-            className="form__input form__input_type_job"
-            placeholder="Краткое описание"
-            type="text"
-            name="form__input_type_job"
-            id="form__input_type_job"
-            minLength="2"
-            maxLength="200"
-            required
-          />
-          <span
-            className="form__input-error"
-            id="form__input_type_job-error"
-          ></span>
-        </fieldset>
-      </PopupWithForm>
+        {/* Попап редактирования профиля */}
+        <PopupWithForm
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          title="Редактировать профиль"
+          name="edit-profile"
+          buttonText="Сохранить"
+        >
+          <fieldset className="form__fieldset">
+            <input
+              className="form__input form__input_type_name"
+              placeholder="Имя пользователя"
+              type="text"
+              name="form__input_type_name"
+              id="form__input_type_name"
+              minLength="2"
+              maxLength="40"
+              required
+            />
+            <span
+              className="form__input-error"
+              id="form__input_type_name-error"
+            ></span>
+            <input
+              className="form__input form__input_type_job"
+              placeholder="Краткое описание"
+              type="text"
+              name="form__input_type_job"
+              id="form__input_type_job"
+              minLength="2"
+              maxLength="200"
+              required
+            />
+            <span
+              className="form__input-error"
+              id="form__input_type_job-error"
+            ></span>
+          </fieldset>
+        </PopupWithForm>
 
-      {/* Попап добавления карточки */}
-      <PopupWithForm
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-        title="Новое место"
-        name="add-card"
-        buttonText="Создать"
-      >
-        <fieldset className="form__fieldset">
-          <input
-            className="form__input form__input_type_place"
-            type="text"
-            name="form__input_type_place"
-            placeholder="Название"
-            id="form__input_type_place"
-            minLength="2"
-            maxLength="30"
-            required
-          />
-          <span
-            className="form__input-error"
-            id="form__input_type_place-error"
-          ></span>
-          <input
-            className="form__input form__input_type_link"
-            type="url"
-            name="form__input_type_link"
-            placeholder="Ссылка на картинку"
-            id="form__input_type_link"
-            required
-          />
-          <span
-            className="form__input-error"
-            id="form__input_type_link-error"
-          ></span>
-        </fieldset>
-      </PopupWithForm>
+        {/* Попап добавления карточки */}
+        <PopupWithForm
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          title="Новое место"
+          name="add-card"
+          buttonText="Создать"
+        >
+          <fieldset className="form__fieldset">
+            <input
+              className="form__input form__input_type_place"
+              type="text"
+              name="form__input_type_place"
+              placeholder="Название"
+              id="form__input_type_place"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+            <span
+              className="form__input-error"
+              id="form__input_type_place-error"
+            ></span>
+            <input
+              className="form__input form__input_type_link"
+              type="url"
+              name="form__input_type_link"
+              placeholder="Ссылка на картинку"
+              id="form__input_type_link"
+              required
+            />
+            <span
+              className="form__input-error"
+              id="form__input_type_link-error"
+            ></span>
+          </fieldset>
+        </PopupWithForm>
 
-      {/* Попап редактирования аватара */}
-      <PopupWithForm
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}
-        title="Обновить аватар"
-        name="update-avatar"
-        buttonText="Сохранить"
-      >
-        <fieldset className="form__fieldset">
-          <input
-            className="form__input form__input_type_avatar"
-            type="url"
-            name="form__input_type_avatar"
-            placeholder="Ссылка на картинку"
-            id="form__input_type_avatar"
-            required
-          />
-          <span
-            className="form__input-error"
-            id="form__input_type_avatar-error"
-          ></span>
-        </fieldset>
-      </PopupWithForm>
+        {/* Попап редактирования аватара */}
+        <PopupWithForm
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          title="Обновить аватар"
+          name="update-avatar"
+          buttonText="Сохранить"
+        >
+          <fieldset className="form__fieldset">
+            <input
+              className="form__input form__input_type_avatar"
+              type="url"
+              name="form__input_type_avatar"
+              placeholder="Ссылка на картинку"
+              id="form__input_type_avatar"
+              required
+            />
+            <span
+              className="form__input-error"
+              id="form__input_type_avatar-error"
+            ></span>
+          </fieldset>
+        </PopupWithForm>
 
-      {/* Попап подтверждения */}
-      <PopupWithForm title="Вы уверены?" name="submit" buttonText="Да" />
+        {/* Попап подтверждения */}
+        <PopupWithForm title="Вы уверены?" name="submit" buttonText="Да" />
 
-      {/* Попап полноразмерного изображения */}
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+        {/* Попап полноразмерного изображения */}
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { api } from '../utils/api';
 import Card from './Card';
+import { CurrentUserContext } from '../../src/contexts/CurrentUserContext';
 
 function Main(props) {
-  const [userName, setUserName] = useState('');
-  const [userDescription, setUserDescription] = useState('');
-  const [userAvatar, setUserAvatar] = useState('');
+  const currentUser = React.useContext(CurrentUserContext);
+
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    api
-      .getUserInfo()
-      .then((userData) => {
-        setUserName(userData.name);
-        setUserDescription(userData.about);
-        setUserAvatar(userData.avatar);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
     api
       .getInitialCards()
       .then((cards) => {
@@ -33,14 +22,18 @@ function Main(props) {
   return (
     <main className="content">
       <section className="profile">
-        <img className="profile__avatar" src={userAvatar} alt="Аватар" />
+        <img
+          className="profile__avatar"
+          src={currentUser.avatar}
+          alt="Аватар"
+        />
 
         <button
           className="profile__update-button"
           onClick={props.onEditAvatar}
         ></button>
 
-        <h1 className="profile__title">{userName}</h1>
+        <h1 className="profile__title">{currentUser.name}</h1>
 
         <button
           className="profile__edit-button"
@@ -49,7 +42,7 @@ function Main(props) {
           onClick={props.onEditProfile}
         ></button>
 
-        <p className="profile__subtitle">{userDescription}</p>
+        <p className="profile__subtitle">{currentUser.about}</p>
 
         <button
           className="profile__add-button"

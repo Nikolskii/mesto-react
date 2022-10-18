39fa96dc29fe8eca8a58d0809369df7dab498225
Denis Nikolskiy <den-apple@ya.ru>
2022-10-18 21:32:17 +0200

    chore: useContext currentUser in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,21 @@
+import React from 'react';
+import { CurrentUserContext } from '../../src/contexts/CurrentUserContext';
+
 function Card({ data, onCardClick }) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = data.owner._id === currentUser._id;
+
+  const cardDeleteButtonClassName = `element__delete ${
+    isOwn ? 'element__delete_visible' : 'element__delete_hidden'
+  }`;
+
+  const isLikes = data.likes.some((like) => like._id === currentUser._id);
+
+  const cardLikeButtonClassName = `element__like ${
+    isLikes ? 'element__like_active' : ''
+  }`;
+
   function handleClick() {
     onCardClick(data);
   }
@@ -15,13 +32,13 @@ function Card({ data, onCardClick }) {
       <h2 className="element__title">{data.name}</h2>
 
       <button
-        className="element__delete"
+        className={cardDeleteButtonClassName}
         type="button"
         aria-label="Удалить"
       ></button>
 
       <button
-        className="element__like"
+        className={cardLikeButtonClassName}
         type="button"
         aria-label="Нравится"
       ></button>
